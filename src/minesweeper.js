e9f4dadc9be7a964b52ac6234f65aae5255c1487
jcmedina <jcmedina@gmail.com>
2018-01-24 07:16:55 -0500

    minesweeper step 2

---
+++ src/minesweeper.js
@@ -0,0 +1,16 @@
+const printBoard = (board) => {
+  console.log('Current: Board');
+  console.log(board[0].join(' | ')); //combines 3 elements from the array within the array. cool!
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+const board  = [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+];
+
+printBoard(board);
+board[0][1] = '1';
+board[2][2] = 'B';
+printBoard(board); 
